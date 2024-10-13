5b221d832d8732a2ba7dbf229cae5915741a71ec
Angel <angelquisit05@gmail.com>
2024-10-13 16:07:45 +0800

    (TEST) Push to test github actions

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  response.send('Initial Request Management Set Up');
+  response.send('Sample push to test github actions');
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
