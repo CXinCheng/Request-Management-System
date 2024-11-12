f79c2971c98e3a8b40c615e8ae370e1ba5b1849e
xincheng-cai <xincheng_cai@apple.com>
2024-11-12 17:24:01 +0800

    feat(front-end): make main body CSS center

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,3 +3,13 @@
         <RouterView />
     </main>
 </template>
+
+<style>
+main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    width: 100%;
+}
+</style>
