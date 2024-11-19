43b54a34688b1cf25470331d7a3cd05d7987d13c
DumbCat-Github <123746718+DumbCat-Github@users.noreply.github.com>
2024-11-19 23:21:28 +0800

    Update router.js
    temporary change for npm run dev to run DashboardView.vue

--- client/src/router.js
+++ client/src/router.js
@@ -1,12 +1,12 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 
 import AuthView from './modules/auth/views/AuthView.vue'
-// import DashboardView from './modules/dashboard/DashboardView.vue'
+import DashboardView from './modules/dashboard/views/DashboardView.vue'
 // import RequestView from './modules/request/RequestView.vue'
 
 const routes = [
-  { path: '/', component: AuthView },
-  //{ path: '/dashboard', component: DashboardView },
+  //{ path: '/', component: AuthView },
+  { path: '/', component: DashboardView },
   //{ path: '/request', component: RequestView },
 ]
 
