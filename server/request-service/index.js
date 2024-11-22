2e53b430eac4980703df3af954e1da475026c879
CXinCheng <Smilence97@outlook.com>
2024-11-22 13:58:20 +0800

    chore: update port number for request service

--- server/request-service/index.js
+++ server/request-service/index.js
@@ -26,7 +26,7 @@ dotenv.config();
 // Routes
 app.use("/api/v1/requests", formRoutes);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
