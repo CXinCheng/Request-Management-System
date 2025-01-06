3359edd817207a7e8c302528409664a5bd5b353b
Jacqueleen Heo <jacqueleenheo@gmail.com>
2025-01-06 23:59:01 +0800

    added identifiers for user service

--- server/auth/index.js
+++ server/auth/index.js
@@ -16,5 +16,5 @@ app.use("/api/v1/auth", authRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+    console.log(`User service is running on port ${PORT}.`);
 });
