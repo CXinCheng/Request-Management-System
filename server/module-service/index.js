ff5272feabfe299b4678c35bbac7d01396f06a68
xincheng-cai <xincheng_cai@apple.com>
2025-01-16 11:31:16 +0800

    feat: API to get class timetable

--- server/module-service/index.js
+++ server/module-service/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import { initialize, close } from './src/configs/db/db.js';
-import { UpdateService } from "./src/services/updateService.js";
+import updateService from "./src/services/updateService.js";
 import moduleRoute from "./src/routes/moduleRoute.js";
 
 const app = express();
@@ -20,7 +20,7 @@ app.use("/api/v1/modules", moduleRoute);
 await initialize();
 
 // Update Module DB
-new UpdateService("2024-2025").updateModule();
+updateService.updateModule();
 
 const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
