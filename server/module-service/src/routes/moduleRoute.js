0eb37811bc64e7327778ba978fc8754deff23844
DumbCat-Github <123746718+DumbCat-Github@users.noreply.github.com>
2025-02-17 21:36:24 +0800

    Added new user_mapping_module API

--- server/module-service/src/routes/moduleRoute.js
+++ server/module-service/src/routes/moduleRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getModuleTimetableByClassNo, getAllModules, updateEducator } from '../controllers/moduleController.js';
+import { getModuleTimetableByClassNo, getAllModules, updateEducator, getUserMappedModules, addUserMappedModule } from '../controllers/moduleController.js';
 
 const router = express.Router();
 
@@ -7,5 +7,9 @@ const router = express.Router();
 router.get('/:moduleCode/:classNo', getModuleTimetableByClassNo);
 router.get('/all', getAllModules);
 router.post('/updateEducator', updateEducator);
+router.get("/user-module-mapping/:userId", getUserMappedModules);
+
+router.get("/user-module-mapping/:userId", getUserMappedModules);  // Fetch user-mapped modules
+router.post("/user-module-mapping", addUserMappedModule);  // Add user to a module
 
 export default router;
